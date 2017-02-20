6f034e0f423b98f1d53e5b9072950d9bf5050eac
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-02-20 17:50:20 +0400

    Changing levelname for issue #53

--- autoxtrabackup.py
+++ autoxtrabackup.py
@@ -137,7 +137,7 @@ def validate_file(file):
               help="Set log level")
 def all_procedure(prepare, backup, partial, verbose, log, defaults_file):
     logger.setLevel(log)
-    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+    formatter = logging.Formatter(fmt='%(asctime)s %([levelname])-8s %(message)s',
                                   datefmt='%Y-%m-%d %H:%M:%S')
     if verbose:
         ch = logging.StreamHandler()
