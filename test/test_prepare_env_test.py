3d38359ad7e89aa64bcf0f8a987657771b7cb4f3
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-06 17:41:25 +0400

    Added all tests for available methods

--- test/test_prepare_env_test.py
+++ test/test_prepare_env_test.py
@@ -1,4 +1,3 @@
-from prepare_env_test_mode.clone_build_start_server import CloneBuildStartServer
 import pytest
 
 @pytest.mark.usefixtures("return_clone_obj")
@@ -8,10 +7,18 @@ class TestBackupTestMode:
     """
 
     def test_clone_percona_qa(self, return_clone_obj):
-        assert return_clone_obj.clone_percona_qa(return_clone_obj.testpath) == True
+        assert return_clone_obj.clone_percona_qa(test_path=return_clone_obj.testpath) == True
 
     def test_clone_ps_server_from_conf(self, return_clone_obj):
-        assert return_clone_obj.clone_ps_server_from_conf(return_clone_obj.git_cmd, return_clone_obj.testpath) == True
+        assert return_clone_obj.clone_ps_server_from_conf(git_cmd=return_clone_obj.git_cmd, test_path=return_clone_obj.testpath) == True
 
     def test_build_server(self, return_clone_obj):
         assert return_clone_obj.build_server(return_clone_obj.testpath) == True
+
+    def test_prepare_startup(self, return_clone_obj):
+        basedir = return_clone_obj.get_basedir(return_clone_obj.testpath)
+        assert return_clone_obj.prepare_startup(basedir_path=basedir, test_path=return_clone_obj.testpath) == True
+
+    def test_start_server(self, return_clone_obj):
+        basedir = return_clone_obj.get_basedir(return_clone_obj.testpath)
+        assert return_clone_obj.start_server(basedir_path=basedir)
