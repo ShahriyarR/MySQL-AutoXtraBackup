76d19cb56f44c2d7f5cd054690a0498c2330dd1d
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-06 17:11:59 +0400

    Added another test for method

--- test/test_prepare_env_test.py
+++ test/test_prepare_env_test.py
@@ -9,3 +9,6 @@ class TestBackupTestMode:
 
     def test_clone_percona_qa(self, return_clone_obj):
         assert return_clone_obj.clone_percona_qa(return_clone_obj.testpath) == True
+
+    def test_clone_ps_server_from_conf(self, return_clone_obj):
+        assert return_clone_obj.clone_ps_server_from_conf(return_clone_obj.git_cmd, return_clone_obj.testpath) == True
