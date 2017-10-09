35791240301901c900c4e6bd1565f5e8a7f7b210
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-09 19:45:37 +0400

    Added unit test for get_basedir()

--- prepare_env_test_mode/clone_build_start_server.py
+++ prepare_env_test_mode/clone_build_start_server.py
@@ -75,6 +75,7 @@ class CloneBuildStartServer:
                 if 'PS' in dir:
                     logger.debug("Could get PS basedir path returning...")
                     basedir_path = "{}/{}"
+                    print(basedir_path.format(self.testpath, dir))
                     return basedir_path.format(self.testpath, dir)
 
         logger.warning("Could not get PS basedir path...")
