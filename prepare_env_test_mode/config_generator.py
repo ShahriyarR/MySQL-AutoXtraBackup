2146f2a5a08318ac305f70c02eb9547ac63d2626
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-11 19:24:03 +0400

    Added [Backup] section generation for config

--- prepare_env_test_mode/config_generator.py
+++ prepare_env_test_mode/config_generator.py
@@ -14,7 +14,7 @@ class ConfigGenerator(CloneBuildStartServer):
 
     def generate_config_files(self):
         # This method for generating separate config files for each XB versions.
-        # TODO: For now we need only generate XB 2.4 version of config with started PS server
+        # TODO: For now we need only generate XB 2.4 version of config with started PS 5.7 server
         # TODO: For future I need to figure out how to generate configs for both version, related to PS version
         conf_2_4 = "xb_2_4.conf"
         conf_path = "{}/{}".format(self.testpath, conf_2_4)
@@ -35,13 +35,39 @@ class ConfigGenerator(CloneBuildStartServer):
                     config.set("{}".format(section1), "#mysql_host", "127.0.0.1")
                     config.set("{}".format(section1), "#mysql_port", "3306")
                     config.set("{}".format(section1), "datadir", "{}/data".format(basedir))
+
+                    section2 = 'Backup'
+                    config.add_section(section2)
+                    config.set(section2, "# Optional: set pid directory")
+                    config.set(section2, "pid_dir", "/tmp/MySQL-AutoXtraBackup")
+                    config.set(section2, "tmp_dir", "/home/shahriyar.rzaev/XB_TEST/mysql_datadirs")
+                    config.set(section2, "#Optional: set warning if pid of backup us running for longer than X")
+                    config.set(section2, "pid_runtime_warning", "2 Hours")
+                    config.set(section2, "backupdir", "/home/shahriyar.rzaev/XB_TEST/backup_dir/ps_5_7") # Can be changed in the future depending on PS version
+                    config.set(section2, "backup_tool", "{}/target/percona-xtrabackup-2.4.x-debug/bin/xtrabackup".format(self.testpath))
+                    config.set(section2, "#Optional: specify different path/version of xtrabackup here for prepare")
+                    config.set(section2, "#prepare_tool", "")
+                    config.set(section2, "xtra_prepare", "--apply-log-only")
+                    config.set(section2, "#Optional: pass additional options for backup stage")
+                    config.set(section2, "#xtra_backup", "--compact")
+                    config.set(section2, "#Optional: pass additional options for prepare stage")
+                    config.set(section2, "#xtra_prepare_options", "--rebuild-indexes")
+                    config.set(section2, "#Optional: pass general additional options; it will go to both for backup and prepare")
+                    config.set(section2, "#xtra_options", "--binlog-info=ON --galera-info")
+                    config.set(section2, "#Optional: set archive and rotation")
+                    config.set(section2, "#archive_dir", "/home/shahriyar.rzaev/XB_TEST/backup_archives")
+                    config.set(section2, "#full_backup_interval", "1 day")
+                    config.set(section2, "#max_archive_size", "100GiB")
+                    config.set(section2, "#max_archive_duration", "4 Days")
+                    config.set(section2, "#Optional WARNING(Enable this if you want to take partial backups). Specify database names or table names.")
+                    config.set(section2, "#partial_list", "test.t1 test.t2 dbtest")
+
                     config.write(cfgfile)
 
         except Exception as err:
-            raise err
-            #logger.error("Failed to generate config file...")
-            #logger.error(err)
-            #return False
+            logger.error("Failed to generate config file...")
+            logger.error(err)
+            return False
         else:
             logger.debug("Config file generated successfully...")
             return True
