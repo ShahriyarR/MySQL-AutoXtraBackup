809c37c54efa588606e5092e23344174d5930c9c
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-12 14:51:02 +0400

    converting to int

--- prepare_env_test_mode/take_backup.py
+++ prepare_env_test_mode/take_backup.py
@@ -7,10 +7,10 @@ class WrapperForBackupTest(CloneBuildStartServer):
 
     def __init__(self):
         super().__init__()
-        self.backup_obj = Backup(config="{}/xb_2_4.conf".format(self.testpath)) 
+        self.backup_obj = Backup(config="{}/xb_2_4.conf".format(self.testpath))
 
 
     def run_all_backup(self):
         # Method for taking backups using backuper.py::all_backup()
-        for _ in range(self.backup_obj.incremental_count + 1):
+        for _ in range(int(self.backup_obj.incremental_count) + 1):
             self.backup_obj.all_backup()
