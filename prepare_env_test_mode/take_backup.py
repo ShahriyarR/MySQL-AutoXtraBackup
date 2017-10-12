0a44138e86bb53cfb1aebffdd50114c42fe157a1
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-12 14:47:17 +0400

    Calling Backup class directly here

--- prepare_env_test_mode/take_backup.py
+++ prepare_env_test_mode/take_backup.py
@@ -1,14 +1,16 @@
 from master_backup_script.backuper import Backup
 from general_conf.generalops import GeneralClass
+from prepare_env_test_mode.clone_build_start_server import CloneBuildStartServer
 
 
-class WrapperForBackupTest(Backup):
+class WrapperForBackupTest(CloneBuildStartServer):
 
     def __init__(self):
-        super().__init__(config="{}/xb_2_4.conf".format(self.testpath))
+        super().__init__()
+        self.backup_obj = Backup(config="{}/xb_2_4.conf")
 
 
     def run_all_backup(self):
         # Method for taking backups using backuper.py::all_backup()
-        for _ in range(self.incremental_count + 1):
-            self.all_backup()
+        for _ in range(self.backup_obj.incremental_count + 1):
+            self.backup_obj.all_backup()
