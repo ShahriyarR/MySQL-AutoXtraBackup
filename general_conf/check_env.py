767c4ada620e768a3b566b85c7e5b1239cfca36e
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-12 15:16:49 +0400

    Trying to fix check_mysql_conf() again and again

--- general_conf/check_env.py
+++ general_conf/check_env.py
@@ -46,7 +46,7 @@ class CheckEnv(GeneralClass):
             return True
 
     def check_mysql_conf(self):
-        if self.mycnf is None:
+        if self.mycnf is None or self.mycnf == '':
             logger.debug("Skipping my.cnf check, because it is not specified")
             return True
         elif not os.path.exists(self.mycnf) and (self.mycnf is not None):
