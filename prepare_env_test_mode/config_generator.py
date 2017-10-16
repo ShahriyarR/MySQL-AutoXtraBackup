b2f2c9632c6ccb2dbd1b02388519a42a86d4c36d
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-17 02:24:48 +0400

    Again faile to generate 5.6 version of xb 2.4 config

--- prepare_env_test_mode/config_generator.py
+++ prepare_env_test_mode/config_generator.py
@@ -159,7 +159,7 @@ class ConfigGenerator(CloneBuildStartServer):
 
         for conf_file in conf_list:
             for basedir in basedirs:
-                if ('5.7' in basedir or '5.6' in basedir) and ('2_4' in conf_file):
+                if (('5.7' in basedir) or ('5.6' in basedir)) and ('2_4' in conf_file):
                     self.generate_config_files(test_path=self.testpath,
                                            conf_file=conf_file,
                                            basedir=basedir,
