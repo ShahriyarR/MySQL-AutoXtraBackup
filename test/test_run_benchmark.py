35ad089eea1c2cc7bceaebc5622dc17044ca1fdf
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2017-10-18 17:54:42 +0400

    Bugfix issue122 (#123)
    * Changed test_get_sock() method
    
    * Changed test_get_mysql_conn() and test_create_db() methods
    
    * Changed test_run_sysbench()
    
    * Finalizing the fix

--- test/test_run_benchmark.py
+++ test/test_run_benchmark.py
@@ -7,13 +7,17 @@ class TestRunBenchmark:
     """
 
     def test_get_sock(self, return_run_benchmark_obj):
-        assert "sock" in return_run_benchmark_obj.get_sock()
+        for basedir in return_run_benchmark_obj.basedir:
+            assert "sock" in return_run_benchmark_obj.get_sock(basedir=basedir)
 
     def test_get_mysql_conn(self, return_run_benchmark_obj):
-        assert "mysql" in return_run_benchmark_obj.get_mysql_conn()
+        for basedir in return_run_benchmark_obj.basedir:
+            assert "mysql" in return_run_benchmark_obj.get_mysql_conn(basedir=basedir)
 
     def test_create_db(self, return_run_benchmark_obj):
-        assert return_run_benchmark_obj.create_db("test_run_benchmark_db") is True
+        for basedir in return_run_benchmark_obj.basedir:
+            assert return_run_benchmark_obj.create_db(db_name="test_run_benchmark_db", basedir=basedir) is True
 
     def test_run_sysbench(self, return_run_benchmark_obj):
-        assert return_run_benchmark_obj.run_sysbench() is True
+        for basedir in return_run_benchmark_obj.basedir:
+            assert return_run_benchmark_obj.run_sysbench(basedir=basedir) is True
