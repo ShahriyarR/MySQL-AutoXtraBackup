145be0e27ebea9772a7d29517867365212f61d99
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-18 19:17:53 +0400

    Changed config generation for backup folders

--- test/test_take_backup.py
+++ test/test_take_backup.py
@@ -8,8 +8,7 @@ class TestTakeBackup:
     def test_run_all_backup(self):
         gen_obj = GeneralClass()
         for conf_files in gen_obj.xb_configs.split():
-            if ('5_7' in conf_files) and ('2_4' in conf_files):
-                obj = WrapperForBackupTest(config='{}/{}'.format(gen_obj.testpath, conf_files))
-                obj.run_all_backup()
+            obj = WrapperForBackupTest(config='{}/{}'.format(gen_obj.testpath, conf_files))
+            obj.run_all_backup()
 
 
