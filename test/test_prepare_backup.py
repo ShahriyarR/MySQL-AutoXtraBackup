35e28a4ce732b50f740e32ef243e0b6d800ef2b5
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-19 17:21:55 +0400

    Added another test for copy_back actions

--- test/test_prepare_backup.py
+++ test/test_prepare_backup.py
@@ -10,3 +10,9 @@ class TestPrepareBackup:
             obj = WrapperForPrepareTest(config='{}/{}'.format(gen_obj.testpath, conf_files))
             obj.run_prepare_backup()
 
+    def test_run_copy_back(self):
+        gen_obj = GeneralClass()
+        for conf_files in gen_obj.xb_configs.split():
+            obj = WrapperForPrepareTest(config='{}/{}'.format(gen_obj.testpath, conf_files))
+            obj.run_copy_back()
+
