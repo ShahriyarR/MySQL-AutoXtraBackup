859605bb77048c1870015b9fddf96c6167d4a700
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-24 18:58:20 +0400

    Preliminary fix for issue #128

--- prepare_env_test_mode/take_backup.py
+++ prepare_env_test_mode/take_backup.py
@@ -3,10 +3,13 @@ from master_backup_script.backuper import Backup
 
 class WrapperForBackupTest(Backup):
 
-    def __init__(self, config='/etc/bck.conf'):
+    def __init__(self, config='/etc/bck.conf', full_dir=None, inc_dir=None):
         self.conf = config
         super().__init__(config=self.conf)
-
+        if full_dir is not None:
+            self.full_dir = full_dir
+        if inc_dir is not None:
+            self.inc_dir = inc_dir
 
     def run_all_backup(self):
         # Method for taking backups using backuper.py::all_backup()
