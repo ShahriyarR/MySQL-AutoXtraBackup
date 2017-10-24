9480de2bbc92ad125236a54cb2e7e6800b501550
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-24 16:19:57 +0400

    Added TODO

--- prepare_env_test_mode/runner_test_mode.py
+++ prepare_env_test_mode/runner_test_mode.py
@@ -18,37 +18,10 @@ class RunnerTestMode(GeneralClass):
         self.basedirs = self.clone_obj.get_basedir()
 
     def wipe_backup_prepare_copyback(self, basedir):
+        # TODO: figure out how to create cycle{num} for each cycle inside backup dir
         for options in ConfigGenerator(config=self.conf).options_combination_generator(self.mysql_options):
             logger.debug("Will start MySQL with {}".format(" ".join(options)))
             if self.clone_obj.wipe_server_all(basedir_path=basedir, options=" ".join(options)):
                 WrapperForBackupTest(config=self.conf).run_all_backup()
                 #WrapperForPrepareTest(config=self.conf).run_prepare_backup()
                 #WrapperForPrepareTest(config=self.conf).copy_back_action()
-
-
-
-        # else:
-        #     logger.debug("Starting test setup from scratch...")
-        #     if self.clone_obj.clone_percona_qa():
-        #         if self.clone_obj.clone_ps_server_from_conf():
-        #             if self.clone_obj.build_server():
-        #                 base_dir = self.clone_obj.get_basedir()
-        #                 if self.clone_obj.prepare_startup(basedir_path=base_dir):
-        #                     if self.clone_obj.start_server(basedir_path=base_dir):
-        #                         url_2_4 = "http://jenkins.percona.com/view/QA/job/qa.pxb24.build/BUILD_TYPE=debug,label_exp=centos7-64/lastSuccessfulBuild/artifact/target/percona-xtrabackup-2.4.x-debug.tar.gz"
-        #                         url_2_3 = "http://jenkins.percona.com/view/QA/job/qa.pxb23.build/BUILD_TYPE=debug,label_exp=centos7-64/lastSuccessfulBuild/artifact/target/percona-xtrabackup-2.3.x-debug.tar.gz"
-        #                         if self.clone_obj.get_xb_packages(url_2_4[-37:], url_2_4):
-        #                             if self.clone_obj.get_xb_packages(url_2_3[-37:], url_2_3):
-        #                                 archive_2_4 = "percona-xtrabackup-2.4.x-debug.tar.gz"
-        #                                 archive_2_3 = "percona-xtrabackup-2.3.x-debug.tar.gz"
-        #                                 if self.clone_obj.extract_xb_archive(file_name=archive_2_4):
-        #                                     if self.clone_obj.extract_xb_archive(file_name=archive_2_3):
-        #                                         # TODO: fix logic here
-        #                                         conf_obj = ConfigGenerator()
-        #                                         if conf_obj.generate_config_files():
-        #                                             if RunBenchmark().run_sysbench():
-        #                                                 # Take backup
-        #                                                 WrapperForBackupTest().run_all_backup()
-        #                                                 WrapperForPrepareTest().run_prepare_backup()
-        #                                                 WrapperForPrepareTest().copy_back_action()
-        #                                                 pass
