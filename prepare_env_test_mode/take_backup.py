d71c13552f2734458757272b1185bcac393bb96a
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-24 19:06:37 +0400

    Trying to override new paths

--- prepare_env_test_mode/take_backup.py
+++ prepare_env_test_mode/take_backup.py
@@ -5,11 +5,12 @@ class WrapperForBackupTest(Backup):
 
     def __init__(self, config='/etc/bck.conf', full_dir=None, inc_dir=None):
         self.conf = config
-        super().__init__(config=self.conf)
         if full_dir is not None:
             self.full_dir = full_dir
         if inc_dir is not None:
             self.inc_dir = inc_dir
+        super().__init__(config=self.conf)
+
 
     def run_all_backup(self):
         # Method for taking backups using backuper.py::all_backup()
