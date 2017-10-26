e08e4bdde770c32759df9fc0ae06805eaaddc4a2
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-26 14:32:21 +0400

    Fixing option passing

--- prepare_env_test_mode/config_generator.py
+++ prepare_env_test_mode/config_generator.py
@@ -144,7 +144,7 @@ class ConfigGenerator(CloneBuildStartServer):
                     config.set(section8, "testpath", "/home/shahriyar.rzaev/XB_TEST/server_dir")
                     config.set(section8, "incremental_count", "3")
                     config.set(section8, "xb_configs", "xb_2_4_ps_5_6.conf xb_2_4_ps_5_7.conf xb_2_3_ps_5_6.conf")
-                    config.set(section8, "default_mysql_options", "--log-bin=mysql-bin,--server-id={},--gtid-mode=ON,--enforce-gtid-consistency,binlog_format=mixed")
+                    config.set(section8, "default_mysql_options", "--log-bin=mysql-bin,--server-id={},--gtid-mode=ON,--enforce-gtid-consistency,--binlog-format=mixed")
                     if '5_7' in conf_file:
                         config.set(section8, "mysql_options",
                                    "--innodb_buffer_pool_size=1G 2G 3G,--innodb_log_file_size=1G 2G 3G,--innodb_page_size=4K 8K 16K 32K 64K")
