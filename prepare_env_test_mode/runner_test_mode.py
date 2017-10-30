9797afeb5cba0ee17b0f92df73f861211f37d19f
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-30 17:13:26 +0400

    Added sleep(10)

--- prepare_env_test_mode/runner_test_mode.py
+++ prepare_env_test_mode/runner_test_mode.py
@@ -118,6 +118,7 @@ class RunnerTestMode(GeneralClass):
             # Start Slave
             self.run_sql_command(start_slave.format(mysql_slave_client_cmd))
             # Check Slave output for errors
+            sleep(10)
             self.check_slave_status(show_slave_status.format(mysql_slave_client_cmd))
         except Exception as err:
             logger.error("Error in run_change_master()")
