80b4de341bf62ca3711f49d701345509c2cd4e9e
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-10-31 17:35:18 +0400

    Adding some fix for sql syntax

--- prepare_env_test_mode/runner_test_mode.py
+++ prepare_env_test_mode/runner_test_mode.py
@@ -103,11 +103,11 @@ class RunnerTestMode(GeneralClass):
 
         dsns_id = randint(10, 99)
         dsns_parent_id = randint(1, 99)
-        dsn="h=localhost,u=root,p='',s='{}'".format(slave_socket)
-        insert_into = "{} -e \'insert into dsns(id, parent_id, dsn) values({}, {}, \'{}\')\'".format(sql_conn,
-                                                                                                 dsns_id,
-                                                                                                 dsns_parent_id,
-                                                                                                 dsn)
+        dsn="h=localhost,u=root,p='',s=\'{}\'".format(slave_socket)
+        insert_into = "{} -e 'insert into dsns(id, parent_id, dsn) values({}, {}, {})'".format(sql_conn,
+                                                                                               dsns_id,
+                                                                                               dsns_parent_id,
+                                                                                               dsn)
         RunnerTestMode.run_sql_command(insert_into)
 
         return True
