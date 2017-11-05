e02268558f1cac05f2f74c22db9b42ccd436367c
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-11-05 17:59:24 +0400

    changed setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     dependency_links=['https://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.4.tar.gz'],
     entry_points='''
         [console_scripts]
-        autoxtrabackup=autoxtrabackup:all_procedure
+        autoxtrabackup=autoxtrabackup
     ''',
     data_files=datafiles,
 )
