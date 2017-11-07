ac11da46197f63c92563ddc803107008774fe7d5
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-11-07 20:24:58 +0400

    prepared test_inc_backup() test

--- test/test_backup.py
+++ test/test_backup.py
@@ -14,8 +14,17 @@ class TestBackup:
                 obj.add_tag(backup_dir=obj.backupdir, backup_name=backup_name, type='Full', tag_string=obj.tag)
 
     def test_full_backup(self):
+        # Method for running full_backup()
         gen_obj = GeneralClass()
         for conf_files in gen_obj.xb_configs.split():
             if '2_3' in conf_files and '5_6' in conf_files:
                 obj = Backup(config='{}/{}'.format(gen_obj.testpath, conf_files), dry_run=0, tag="My first full backup")
                 obj.full_backup()
+
+    def test_inc_backup(self):
+        # Method for running inc_backup()
+        gen_obj = GeneralClass()
+        for conf_files in gen_obj.xb_configs.split():
+            if '2_3' in conf_files and '5_6' in conf_files:
+                obj = Backup(config='{}/{}'.format(gen_obj.testpath, conf_files), dry_run=0, tag="My first inc backup")
+                obj.inc_backup()
