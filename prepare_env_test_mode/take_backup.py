59a0985c024a759ba6b4c485b16fca0f11932e11
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-11-16 21:33:02 +0400

    Preliminary fix for issue #190

--- prepare_env_test_mode/take_backup.py
+++ prepare_env_test_mode/take_backup.py
@@ -22,8 +22,6 @@ class WrapperForBackupTest(Backup):
             for i in range(1, 10):
                 sql_encrypt = "alter table sysbench_test_db.sbtest{} encryption='Y'".format(i)
                 RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=sql_encrypt)
-                flush_tables = "flush tables"
-                RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=flush_tables)
                 # Compression related issue -> https://bugs.launchpad.net/percona-xtrabackup/+bug/1641745
                 # Disabling for now
                 # TODO: Enable this after #1641745 is fixed.
@@ -31,7 +29,18 @@ class WrapperForBackupTest(Backup):
                 # RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=sql_compress)
                 # sql_optimize = "optimize table sysbench_test_db.sbtest{}".format(i)
                 # RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=sql_optimize)
+            for i in range(10, 15):
+                sql_alter = "alter table sysbench_test_db.sbtest{} engine=rocksdb"
+                RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=sql_alter)
+
+        for i in range(15, 20):
+            sql_alter = "alter table sysbench_test_db.sbtest{} engine=tokudb"
+            RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=sql_alter)
+
+        flush_tables = "flush tables"
+        RunBenchmark.run_sql_statement(basedir=self.basedir, sql_statement=flush_tables)
         sleep(20)
+
         for _ in range(int(self.incremental_count) + 1):
             RunBenchmark().run_sysbench_run(basedir=self.basedir)
             self.all_backup()
