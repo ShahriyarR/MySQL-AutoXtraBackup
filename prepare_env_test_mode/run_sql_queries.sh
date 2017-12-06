b73f50d0a9652f9553e23c25ea9d96e077a09632
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-12-06 20:17:40 +0400

    Do not redirect to /dev/null for now

--- prepare_env_test_mode/run_sql_queries.sh
+++ prepare_env_test_mode/run_sql_queries.sh
@@ -6,4 +6,5 @@ SQL=$3
 MYSQL_USER=root
 
 echo "Running queries using MYSQL_SOCK=${MYSQL_SOCK}"
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "${SQL}" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "${SQL}"
+#> /dev/null 2>&1
