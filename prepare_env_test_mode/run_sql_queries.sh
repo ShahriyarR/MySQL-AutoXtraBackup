7572d6501a86abf81e0726258125cc701d4a74e1
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-12-08 01:29:01 +0400

    Finalizing the feature implementation

--- prepare_env_test_mode/run_sql_queries.sh
+++ prepare_env_test_mode/run_sql_queries.sh
@@ -5,6 +5,5 @@ MYSQL_SOCK=$2
 SQL=$3
 MYSQL_USER=root
 
-echo "Running queries using MYSQL_SOCK=${MYSQL_SOCK}"
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "${SQL}"
-#> /dev/null 2>&1
+#echo "Running queries using MYSQL_SOCK=${MYSQL_SOCK}"
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "${SQL}" > /dev/null 2>&1
