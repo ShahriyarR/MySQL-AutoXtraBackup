e9e039483785c68db57af2601c6d602c000d3b59
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-12-20 15:59:04 +0400

    Removed sleep statement

--- prepare_env_test_mode/ddl_test.sh
+++ prepare_env_test_mode/ddl_test.sh
@@ -13,7 +13,6 @@ ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create uniq
 #echo "drop table if exists sysbencht_test_db.sb1"|./use
 #echo "create table sb1 as select id,c from sbtest1 where id < 150000;"|./use db1
 #echo "create unique index ix on sb1 (id)"|./use db1
-sleep 1
 ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "drop table if exists sysbench_test_db.sb2" > /dev/null 2>&1
 ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table sysbench_test_db.sb2(id int(11) NOT NULL, c char(120) NOT NULL DEFAULT '')" > /dev/null 2>&1
 ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "insert into sysbench_test_db.sb2 select id, c from sysbench_test_db.sbtest1" > /dev/null 2>&1
