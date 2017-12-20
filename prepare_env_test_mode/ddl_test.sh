e71ce5da71f349c5004e2a9e08dfe74c699d4621
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2017-12-20 15:04:30 +0400

    fixing redirect issue

--- prepare_env_test_mode/ddl_test.sh
+++ prepare_env_test_mode/ddl_test.sh
@@ -6,18 +6,18 @@ MYSQL_USER=root
 #CONN_STR=${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK}
 
 
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "drop table if exists sysbench_test_db.sb1" /dev/null 2>&1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table sysbench_test_db.sb1(id int(11) NOT NULL, c char(120) NOT NULL DEFAULT '')" /dev/null 2>&1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "insert into sysbench_test_db.sb1 select id, c from sysbench_test_db.sbtest1" /dev/null 2>&1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create unique index ix on sysbench_test_db.sb1 (id)" /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "drop table if exists sysbench_test_db.sb1" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table sysbench_test_db.sb1(id int(11) NOT NULL, c char(120) NOT NULL DEFAULT '')" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "insert into sysbench_test_db.sb1 select id, c from sysbench_test_db.sbtest1" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create unique index ix on sysbench_test_db.sb1 (id)" > /dev/null 2>&1
 #echo "drop table if exists sysbencht_test_db.sb1"|./use
 #echo "create table sb1 as select id,c from sbtest1 where id < 150000;"|./use db1
 #echo "create unique index ix on sb1 (id)"|./use db1
 sleep 1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "drop table if exists sysbench_test_db.sb2" /dev/null 2>&1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table sysbench_test_db.sb2(id int(11) NOT NULL, c char(120) NOT NULL DEFAULT '')" /dev/null 2>&1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "insert into sysbench_test_db.sb2 select id, c from sysbench_test_db.sbtest1" /dev/null 2>&1
-${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create unique index ix on sysbench_test_db.sb2 (id)" /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "drop table if exists sysbench_test_db.sb2" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create table sysbench_test_db.sb2(id int(11) NOT NULL, c char(120) NOT NULL DEFAULT '')" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "insert into sysbench_test_db.sb2 select id, c from sysbench_test_db.sbtest1" > /dev/null 2>&1
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} -e "create unique index ix on sysbench_test_db.sb2 (id)" > /dev/null 2>&1
 #echo "drop table if exists db2.sb1"|./use
 #echo "create table sb1 as select id,c from sbtest1 where id < 150000;"|./use db2
 #echo "create unique index ix on sb1 (id)"|./use db2
