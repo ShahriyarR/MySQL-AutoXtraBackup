534f233fd2b4c34f8551418fbc2d824df64ca76a
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2018-05-16 18:03:56 +0400

    Preliminary fix for issue #278

--- prepare_env_test_mode/call_create_index_temp.sh
+++ prepare_env_test_mode/call_create_index_temp.sh
@@ -4,4 +4,4 @@ PATH=$1
 BASEDIR=$2
 MYSQL_SOCK=$3
 
-while true; do /usr/bin/bash ${PATH}/create_index_temp.sh ${BASEDIR} ${MYSQL_SOCK}; done
+while true; do /bin/bash ${PATH}/create_index_temp.sh ${BASEDIR} ${MYSQL_SOCK}; done
--- prepare_env_test_mode/call_temp_table_test.sh
+++ prepare_env_test_mode/call_temp_table_test.sh
@@ -4,4 +4,4 @@ PATH=$1
 BASEDIR=$2
 MYSQL_SOCK=$3
 
-while true; do /usr/bin/bash ${PATH}/temp_table_test.sh ${BASEDIR} ${MYSQL_SOCK}; done
+while true; do /bin/bash ${PATH}/temp_table_test.sh ${BASEDIR} ${MYSQL_SOCK}; done
