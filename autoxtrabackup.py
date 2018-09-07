ac28916ad509cbe40311e188d338c735ff5c9dde
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2018-09-07 12:12:11 +0400

    Version Bump

--- autoxtrabackup.py
+++ autoxtrabackup.py
@@ -47,7 +47,7 @@ def print_version(ctx, param, value):
     click.echo("Email: rzayev.shahriyar@yandex.com")
     click.echo(
         "Based on Percona XtraBackup: https://github.com/percona/percona-xtrabackup/")
-    click.echo('MySQL-AutoXtraBackup Version: 1.5.2')
+    click.echo('MySQL-AutoXtraBackup Version: 1.5.3')
     ctx.exit()
 
 
