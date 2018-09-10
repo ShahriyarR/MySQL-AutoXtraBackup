e748cfefa89134967ef02993642c894989c8bbca
mchawla16 <mchawla16@yahoo.com>
2018-09-10 16:55:36 +0000

    PQA-305:Fixes in call_innodb_online_alter_encryption_alters.sh

--- prepare_env_test_mode/call_innodb_online_alter_encryption_alters.sh
+++ prepare_env_test_mode/call_innodb_online_alter_encryption_alters.sh
@@ -8,4 +8,13 @@ MYSQL_SOCK=$2
 FILE_DIR=$3
 MYSQL_USER=root
 
-while true; do ${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} < ${FILE_DIR}/innodb_online_alter_encryption_alters.sql >> /dev/null 2>&1; done
+while true; do 
+# MC: The sql exits when there is an error, hence added the --force option so that it will keep on executing
+${BASEDIR}/bin/mysql --user=${MYSQL_USER} --socket=${MYSQL_SOCK} --force < ${FILE_DIR}/innodb_online_alter_encryption_alters.sql >> /dev/null 2>&1 
+
+# MC: This script must exit after backup in a cycle has completed otherwise there will be multiple processes running, each cycle executes this script in a new process.
+${BASEDIR}/bin/mysqladmin ping --user=${MYSQL_USER} --socket=${MYSQL_SOCK} 2>/dev/null 1>&2
+if [ $? -ne 0 ]; then
+exit 0
+fi
+done
