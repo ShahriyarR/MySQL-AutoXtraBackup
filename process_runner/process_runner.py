e3a4d7c2af7c3212edee236119c9466d642f0c98
Mark Stoute <mstoute@eyereturn.com>
2019-01-14 14:05:43 -0500

    make process_runner docstring more generic

--- process_runner/process_runner.py
+++ process_runner/process_runner.py
@@ -24,27 +24,27 @@ class ProcessRunner(GeneralClass):
         GeneralClass.__init__(self, self.conf)
 
     @staticmethod
-    def run_command(xtrabackup_command):
+    def run_command(command):
         """
-        executes a prepared xtrabackup command
+        executes command, enables real-time console & log output.
 
-        This function unifies calls to xtrabackup, and to enables real-time console & log output.
+        This function should eventually be used for all subprocess calls.
 
-        :param xtrabackup_command: bash 'xtrabackup' command to be executed
-        :type xtrabackup_command: str
+        :param command: bash command to be executed (string)
+        :type command: str
         :return: True if success, False if failure
         :rtype: bool
         """
         # filter out password from argument list, print command to execute
-        filtered_xtrabackup_cmd = re.sub("--password='?\w+'?", "--password='*'", xtrabackup_command)
-        logger.debug("SUBPROCESS STARTING: {}".format(filtered_xtrabackup_cmd))
+        filtered_command = re.sub("--password='?\w+'?", "--password='*'", command)
+        logger.debug("SUBPROCESS STARTING: {}".format(filtered_command))
 
         # start the xtrabackup process
-        process = subprocess.Popen(xtrabackup_command, stdout=PIPE, stderr=STDOUT, shell=True)
+        process = subprocess.Popen(command, stdout=PIPE, stderr=STDOUT, shell=True)
         logger.debug("SUBPROCESS PID: {}".format(process.pid))
 
         # real time logging/stdout output
-        cmd_root = filtered_xtrabackup_cmd.split(" ")[0].split("/")[-1]
+        cmd_root = filtered_command.split(" ")[0].split("/")[-1]
         for line in iter(process.stdout.readline, b''):
             fixed_line = line.decode("utf-8")
             sys.stdout.write(fixed_line)
