0ca0be3e5f1c5a009fb4ac3bcdb89b4d24c74f55
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-06-27 12:44:22 +0400

    Adding type hints to GeneralClass - still need to check with mypy

--- general_conf/generalops.py
+++ general_conf/generalops.py
@@ -1,21 +1,22 @@
 import configparser
 from os.path import isfile
-import humanfriendly
+import humanfriendly  # type: ignore
 import logging
 from general_conf import path_config
+from typing import Dict
 logger = logging.getLogger(__name__)
 
 
 class GeneralClass:
 
-    def __init__(self, config=path_config.config_path_file):
+    def __init__(self, config: str = path_config.config_path_file) -> None:
         if isfile(config):
             self.con = configparser.ConfigParser()
             self.con.read(config)
         else:
             logger.critical("Missing config file : {}".format(path_config.config_path_file))
 
-    def mysql_options(self):
+    def mysql_options(self) -> Dict[str, str]:
         section = 'MySQL'
         return {'mysql': self.con.get(section, 'mysql'),
                 'mycnf': self.con.get(section, 'mycnf'),
@@ -27,13 +28,13 @@ class GeneralClass:
                 'mysql_port': self.con.get(section, 'mysql_port'),
                 'data_dir': self.con.get(section, 'data_dir')}
 
-    def logging_options(self):
+    def logging_options(self) -> Dict[str, str]:
         section = 'Logging'
         return {'log_level': self.con.get(section, 'log_level'),
                 'log_file_max_bytes': self.con.get(section, 'log_file_max_bytes'),
                 'log_file_backup_count': self.con.get(section, 'log_file_backup_count')}
 
-    def compression_options(self):
+    def compression_options(self) -> Dict[str, str]:
         section = 'Compress'
         return {'compress': self.con.get(section, 'compress'),
                 'compress_chunk_size': self.con.get(section, 'compress_chunk_size'),
@@ -41,20 +42,20 @@ class GeneralClass:
                 'decompress': self.con.get(section, 'decompress'),
                 'remove_original': self.con.get(section, 'remove_original')}
 
-    def xbstream_options(self):
+    def xbstream_options(self) -> Dict[str, str]:
         section = 'Xbstream'
         return {'xbstream': self.con.get(section, 'xbstream'),
                 'stream': self.con.get(section, 'stream'),
                 'xbstream_options': self.con.get(section, 'xbstream_options'),
                 'xbs_decrypt': self.con.get(section, 'xbs_decrypt')}
 
-    def command_options(self):
+    def command_options(self) -> Dict[str, str]:
         section = 'Commands'
         return {'start_mysql_command': self.con.get(section, 'start_mysql_command'),
                 'stop_mysql_command': self.con.get(section, 'stop_mysql_command'),
                 'chown_command': self.con.get(section, 'chown_command')}
 
-    def encryption_options(self):
+    def encryption_options(self) -> Dict[str, str]:
         section = 'Encrypt'
         return {'xbcrypt': self.con.get(section, 'xbcrypt'),
                 'encrypt': self.con.get(section, 'encrypt'),
@@ -65,7 +66,7 @@ class GeneralClass:
                 'decrypt': self.con.get(section, 'decrypt'),
                 'remove_original': self.con.get(section, 'remove_original')}
 
-    def backup_archive_options(self):
+    def backup_archive_options(self) -> Dict[str, str]:
         section = 'Backup'
         # backward compatible with old config 'max_archive_size' and newer 'archive_max_size'
         archive_max_size = self.con.get(section, 'max_archive_size')
@@ -84,14 +85,15 @@ class GeneralClass:
         return {'archive_dir': self.con.get(section, 'archive_dir'),
                 'prepare_archive': self.con.get(section, 'prepare_archive'),
                 'move_archive': self.con.get(section, 'move_archive'),
-                'archive_max_size': archive_max_size,
-                'archive_max_duration': archive_max_duration}
+                'archive_max_size': archive_max_size,  # type: ignore
+                'archive_max_duration': archive_max_duration  # type: ignore
+                }
 
-    def backup_options(self):
+    def backup_options(self) -> Dict[str, str]:
         section = 'Backup'
         return {'pid_dir': self.con.get(section, 'pid_dir', fallback='/tmp/'),
                 'tmp_dir': self.con.get(section, 'tmp_dir'),
-                'pid_runtime_warning': humanfriendly.parse_timespan(self.con.get(section, 'pid_runtime_warning')),
+                'pid_runtime_warning': humanfriendly.parse_timespan(self.con.get(section, 'pid_runtime_warning')),  # type: ignore
                 'backup_dir': self.con.get(section, 'backup_dir'),
                 'full_dir': self.con.get(section, 'backup_dir') + '/full',
                 'inc_dir': self.con.get(section, 'backup_dir') + '/inc',
@@ -101,5 +103,5 @@ class GeneralClass:
                 'xtra_prepare_options': self.con.get(section, 'xtra_prepare_options'),
                 'xtra_options': self.con.get(section, 'xtra_options'),
                 'full_backup_interval': humanfriendly.parse_timespan(self.con.get(section, 'full_backup_interval',
-                                                                                  fallback=86400)),
+                                                                                  fallback=86400)),  # type: ignore
                 'partial_list': self.con.get(section, 'partial_list')}
