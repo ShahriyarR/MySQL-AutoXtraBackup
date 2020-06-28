386ae93922fc059c4a8d6ebe67253daa131efc5f
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-06-28 13:34:51 +0400

    Encountered another error here

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-
-
-
-from general_conf.generalops import GeneralClass
-from backup_backup.backuper import Backup
-from general_conf.check_env import CheckEnv
-from prepare_env_test_mode.runner_test_mode import RunnerTestMode
-from .autoxtrabackup import *
-
+#
+#
+#
+# from general_conf.generalops import GeneralClass
+# from backup_backup.backuper import Backup
+# from general_conf.check_env import CheckEnv
+# from prepare_env_test_mode.runner_test_mode import RunnerTestMode
+# from .autoxtrabackup import *
+#
