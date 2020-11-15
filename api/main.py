f88244177123ca727518d3170542620e89f931b2
Shahriyar Rzayev <rzayev.shahriyar@yandex.com>
2020-11-15 22:50:47 +0400

    Prepare backup api changes (#422)
    * Trying to change response status codes
    
    * Added /prepare endpoint

--- api/main.py
+++ api/main.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 from backup_backup.backuper import Backup
+from backup_prepare.prepare import Prepare
+
 
 app = FastAPI()
 
 
 @app.post("/backup")
-async def backup():
+async def backup() -> JSONResponse:
     backup_ = Backup()
     result = backup_.all_backup()
     if result:
-        return JSONResponse({"result": "Successfully finished the backup process"})
-    return JSONResponse({"result": "[FAILED] to take backup"})
+        return JSONResponse(content={"result": "Successfully finished the backup process"},
+                            status_code=status.HTTP_201_CREATED)
+    return JSONResponse(content={"result": "[FAILED] to take backup"},
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+@app.post("/prepare")
+async def prepare() -> JSONResponse:
+    prepare_ = Prepare()
+    result = prepare_.prepare_inc_full_backups()
+    if result:
+        return JSONResponse(content={"result": "Successfully prepared all the backups"},
+                            status_code=status.HTTP_200_OK)
+    return JSONResponse(content={"result": "[FAILED] to prepare backup"},
+                        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
