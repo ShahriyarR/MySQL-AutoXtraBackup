4522c007b6652b6842d810cbbb132463682e0552
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2020-11-15 20:32:57 +0400

    Trying to change response status codes

--- api/main.py
+++ api/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
 from backup_backup.backuper import Backup
 
 app = FastAPI()
 
 
-@app.post("/backup")
-async def backup():
+@app.post("/backup", status_code=201)
+async def backup(response: Response):
     backup_ = Backup()
     result = backup_.all_backup()
     if result:
         return JSONResponse({"result": "Successfully finished the backup process"})
+    response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     return JSONResponse({"result": "[FAILED] to take backup"})
-
