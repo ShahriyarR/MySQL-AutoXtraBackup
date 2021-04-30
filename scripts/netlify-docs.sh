0fc9102448f16135bd35c316c8cdac5378e1a9b0
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2021-04-30 20:43:59 +0400

    trying to owercome the issue with netlify install

--- scripts/netlify-docs.sh
+++ scripts/netlify-docs.sh
@@ -9,6 +9,6 @@ cd -
 # Install Flit to be able to install all
 python3 -m pip install --user flit
 # Install with Flit
-python3 -m flit install --user --extras doc
+python3 -m flit install --user --deps=production
 # Finally, run sphinx make
 python3 -m make html
