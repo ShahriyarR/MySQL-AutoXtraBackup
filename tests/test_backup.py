08301a9581389d086cc7d9274d9695b957ee62b3
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2021-05-05 21:20:45 +0400

    First batch of tests

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -6,10 +6,6 @@ import pytest
 
 @pytest.mark.usefixtures("return_bck_obj")
 class TestBackup:
-    def test_create_mysql_client_command(self, return_bck_obj):
-        result = '/usr/bin/mysql --defaults-file= -uroot --password=12345 --socket=/var/lib/mysql/mysql.sock -e "select 1"'
-        sql = "select 1"
-        assert return_bck_obj.create_mysql_client_command(sql) == result
 
     def test_full_backup_without_tag(self, return_bck_obj):
         return_bck_obj.clean_full_backup_dir()
@@ -23,6 +19,6 @@ class TestBackup:
         # Making it None back for global object
         return_bck_obj.tag = None
         # Check if the backup tag file is created and contains given string
-        assert os.path.isfile("{}/backup_tags.txt".format(return_bck_obj.backupdir))
-        with open("{}/backup_tags.txt".format(return_bck_obj.backupdir), "r") as file:
+        assert os.path.isfile("{}/backup_tags.txt".format(return_bck_obj.builder_obj.backup_options.get('backup_dir')))
+        with open("{}/backup_tags.txt".format(return_bck_obj.builder_obj.backup_options.get('backup_dir')), "r") as file:
             assert "My first full backup" in file.read()
